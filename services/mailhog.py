301e76fb5ec3184e1ace35d183982a86ccc98bb8
migushova <ned-ns@bk.ru>
2023-05-02 22:35:08 +0300

    добавление клиента для почтового сервиса

---
+++ services/mailhog.py
@@ -0,0 +1,29 @@
+import json
+from requests import session, Response
+
+
+class MailHogApi:
+    def __init__(self, host="http://localhost:5025", headers=None):
+        self.host = host
+        self.session = session()
+
+    def get_api_v2_messages(self, limit=50) -> Response:
+        """
+        Get messages by limit
+        :param limit:
+        :return:
+        """
+        response = self.session.get(
+            url=f"{self.host}/api/v2/messages",
+            params={
+                'limit': limit
+            }
+        )
+
+        return response
+
+    def get_token_from_last_email(self) -> str:
+        emails = self.get_api_v2_messages(limit=1).json()
+        token_url = json.loads(emails['items'][0]['Content']['Body'])['ConfirmationLinkUrl']
+        token = token_url.split('/')[-1]
+        return token
--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -1,15 +1,17 @@
-import requests
 from services.dm_api_account import DmApiAccount
+from services.mailhog import MailHogApi
 
 
 def test_post_v1_account():
+    mailhog = MailHogApi(host="http://localhost:5052")
     api = DmApiAccount(host="http://localhost:5051")
     json = {
-        "login": "login774",
-        "email": "login774@mail.ru",
-        "password": "login774"
+        "login": "login768",
+        "email": "login768@mail.ru",
+        "password": "login768"
     }
-    response = api.account.post_v1_account(
-        json=json
-    )
+    response = api.account.post_v1_account(json=json)
+    token = mailhog.get_token_from_last_email()
+    response = api.account.put_v1_account_token(token=token)
     print(response)
+    print(response.json())
